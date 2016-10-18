a8f36901cd86f75efe9a3f5732ab613d818cdd54
Krister <krister.torsvik@gmail.com>
2016-10-18 11:09:09 +0200

    small refactor

--- bonus-2/scripts/bonus2.js
+++ bonus-2/scripts/bonus2.js
@@ -1,16 +1,9 @@
-
-//Paste your code from task-2.js here :)
 (function() {
   'use strict'
   var list = document.querySelector('ul');
   var inputText = document.querySelector('input[type="text"]');
+  var formElement = document.querySelector('form');
   var array = getStoredTasks();
-
-  function buttonClickHandler(clickEvent){
-  	console.log("Got clicked");
-  }
-  var buttonElement = document.querySelector('input[type="submit"]');
-  buttonElement.addEventListener('click', buttonClickHandler);
 
   function submitFormHandler(event) {
     if( inputText.value === "") {
@@ -23,7 +16,6 @@
     inputText.value = "";
     event.preventDefault();
   }
-  var formElement = document.querySelector('form');
   formElement.addEventListener('submit', submitFormHandler);
 
   function createTaskElement(description, done) {
@@ -35,20 +27,18 @@
       for(var i = 0; i < array.length; i++) {
         if(array[i].description === label.textContent && array[i].done === true) {
             array[i].done = false;
-            console.log(array[i]);
         }
         else if (array[i].description === label.textContent && array[i].done === false) {
           array[i].done = true;
-          console.log(array[i]);
         }
+
         if(done.done === true) {
           checkbox.checked = true;
         } else {
-          checkbox.checked = false;
+           checkbox.checked = false;
         }
       }
       storeAllTasks();
-      return checkbox;
     });
     checkbox.checked = done.done;
     label.textContent = description.description;
@@ -60,17 +50,14 @@
   function addTasks(description) {
     var todoObj = { description: description, done: false };
     array.push(todoObj);
-    return array;
   }
 
   function renderTasks() {
-    var checkbox = document.querySelector('input[type="checkbox"]');
-    var listItem;
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
     for(var i = 1; i <= array.length; i++) {
-      listItem = createTaskElement(array[i - 1], array[i - 1]);
+      var listItem = createTaskElement(array[i - 1], array[i - 1]);
       list.appendChild(listItem);
     }
   }
